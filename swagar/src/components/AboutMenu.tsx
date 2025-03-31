f029561d9945c49e00f302fca2984e4f779b93a9
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-03-31 19:42:50 +0530

    about section done too home page full done

--- swagar/src/components/AboutMenu.tsx
+++ swagar/src/components/AboutMenu.tsx
@@ -1,19 +1,27 @@
 import FlowingMenu from "./FlowingMenu/FlowingMenu";
 
-
-
 const demoItems = [
   { link: '#', text: 'Virtual', image: 'https://i.pinimg.com/736x/46/5f/da/465fdae0ede26b8728cc8291ecd93195.jpg' },
-  { link: '#', text: 'Tryon', image: 'https://i.pinimg.com/736x/09/35/30/0935305b6c80bcb8028829c38a162dd8.jpg' },
- 
+  { link: '#', text: 'Tryon', image: 'https://i.pinimg.com/736x/09/35/30/0935305b6c80bbcb8028829c38a162dd8.jpg' },
 ];
 
 export default function AboutMenu() {
   return (
-
-<div style={{ height: '600px', position: 'relative' }}>
-  <FlowingMenu items={demoItems} />
-</div>
-
+    <div className="flex flex-col items-center justify-center bg-black text-white p-8" style={{ minHeight: '600px' }}>
+      {/* Flowing menu container */}
+      <div className="w-full" style={{ height: '300px', position: 'relative' }}>
+        <FlowingMenu items={demoItems} />
+      </div>
+      {/* About Us text section */}
+      <div className="mt-8 max-w-3xl text-center">
+        <h2 className="text-3xl font-bold mb-4">About Swaggar</h2>
+        <p className="text-lg mb-2">
+          Swaggar is revolutionizing the fashion industry by merging cutting-edge technology with style. Our platform allows you to virtually try on clothes using advanced 3D models, AI-driven sizing, and augmented reality.
+        </p>
+        <p className="text-lg">
+          We are committed to making shopping fun, interactive, and tailored to your personal style. With our innovative approach, you can see exactly how each piece fits and looks on you before making a purchase. Join us as we redefine the future of fashion!
+        </p>
+      </div>
+    </div>
   );
 }
--- swagar/src/components/AutoPlay.tsx
+++ swagar/src/components/AutoPlay.tsx
@@ -11,8 +11,8 @@ export function AutoPlay() {
         className="p-1 md:p-4 border rounded-3xl dark:bg-neutral-900 dark:border-neutral-800 mx-auto w-3/4 h-1/2 md:h-3/4"
       >
         <Compare
-          firstImage="https://i.pinimg.com/736x/72/8d/81/728d8103e763bd135ae5ef549aaac298.jpg"
-          secondImage="https://i.pinimg.com/474x/e8/78/ac/e878ac1cb6d26e37a482ac9c0550b394.jpg"
+          firstImage="https://i.pinimg.com/736x/50/1e/2c/501e2cde12fb5a4e93d4947a957542bc.jpg"
+          secondImage="https://i.pinimg.com/736x/36/79/6f/36796f6d7312080e18b95f5cb88a2aa9.jpg"
           firstImageClassName="object-cover object-left-top w-full"
           secondImageClassname="object-cover object-left-top w-full"
           className="w-full h-full rounded-[22px] md:rounded-lg"
